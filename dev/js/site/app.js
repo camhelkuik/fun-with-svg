1148c64c6b3238235fc20889ecf7382195c2e942
Cameron Helkuik <cameronhelkuik@gmail.com>
2015-08-19 16:18:20 -0500

    Got the gears to work

--- dev/js/site/app.js
+++ dev/js/site/app.js
@@ -2,16 +2,17 @@ window.onload = setupDom;
 
 
 function setupDom(){
-  // changeAllTheSources();
-  var graphic = document.getElementsByClassName("graph")[0];
-  console.log(graphic);
+
+  var graphic = document.getElementsByClassName("gears")[0];
   var graphicDoc = graphic.contentDocument;
+    
+  var orangeGear = graphicDoc.getElementById("top-gear");
+  var grayGear = graphicDoc.getElementById("bottom-gear");
+  var bigGear = graphicDoc.getElementById("big-gear");
   
-  var graphLines = graphicDoc.getElementById("dots-lines");
-  console.log(graphLines);
-//
-//   TweenMax.from(graphLines, 1, {tranformOrigin: '100% 100%', rotation: -45});
-//   TweenMax.to(graphLines, .5, {transformOrigin: '50% 50%', scale: 1.2, delay: .9});
+  TweenMax.to(bigGear, 3, {rotation:360, transformOrigin:"50% 50%"});
+  TweenMax.to(orangeGear, 3, {rotation:-360, transformOrigin:"50% 50%"});
+  TweenMax.to(grayGear, 3, {rotation:-360, transformOrigin:"50% 50%"});
   
 }
 
